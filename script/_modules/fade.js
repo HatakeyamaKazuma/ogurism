c0e626142bb3129007f78ed8429df5491604d6d7
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-06 11:45:07 +0900

    Create fade.js

---
+++ script/_modules/fade.js
@@ -0,0 +1,32 @@
+export default function fade() {
+    document.addEventListener('DOMContentLoaded', function () {
+        const fadeBackground = document.querySelectorAll('.fade__bg');
+        const fadeAnime = document.querySelectorAll('.fade');
+
+        const fadeBackgroundObserver = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('is-active');
+                } else {
+                    entry.target.classList.remove('is-active');
+                }
+            });
+        }, {
+            threshold: 0.3  // 要素の10%が表示されたら発火
+        });
+
+        const fadeAnimeObserver = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('is-active');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.1  // 要素の10%が表示されたら発火
+        });
+
+        fadeBackground.forEach(el => fadeBackgroundObserver.observe(el));
+        fadeAnime.forEach(el => fadeAnimeObserver.observe(el));
+    });
+}
