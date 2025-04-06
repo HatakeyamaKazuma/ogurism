4181576c527e43d25fc85ca219db6f41db6ebfc5
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-06 20:52:25 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -154,8 +154,6 @@ function sideScrollFunc() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.innerWidth <= 750) {
 function slider() {
     const businessSwiper = new Swiper(".business__swiper", {
         loop: true, // スライドをループさせる
@@ -193,47 +191,21 @@ function slider() {
         slidesPerView: 1.4,//枚数指定
     });
 }
+
+hamburgerFunc();
+accordionFunc();
+fadeFunc();
+heroHeaderFunc();
+sideScrollFunc();
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth <= 750) {
+        slider();
     }
 })
 
 document.addEventListener("resize", function () {
     if (window.innerWidth <= 750) {
-function slider() {
-    const businessSwiper = new Swiper(".business__swiper", {
-        loop: true, // スライドをループさせる
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true, // ページネーションをクリック可能に
-        },
-        effect: 'slide', // スライドのエフェクト（デフォルト）
-        speed: 800, // スライドの速度（ミリ秒）
-        centeredSlides: true,//真ん中寄せ
-        slidesPerView: 1.4,//枚数指定
-    });
-
-    const artistSwiper = new Swiper(".artist__swiper", {
-        loop: true, // スライドをループさせる
-        // autoplay: {
-        //     delay: 3000, // 3秒ごとに自動スライド
-        //     disableOnInteraction: false, // ユーザー操作後も自動再生を継続
-        // },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true, // ページネーションをクリック可能に
-        },
-        effect: 'slide', // スライドのエフェクト（デフォルト）
-        speed: 800, // スライドの速度（ミリ秒）
-        centeredSlides: true,//真ん中寄せ
-        slidesPerView: 1.4,//枚数指定
-    });
-}
+        slider();
     }
 })
