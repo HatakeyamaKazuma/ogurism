aaf2822c91ffa81dfad05dca85741c59bd214116
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-06 11:45:19 +0900

    Create hamburger.js

---
+++ script/_modules/hamburger.js
@@ -0,0 +1,21 @@
+export default function hamburgerFunc() {
+    //ここにハンバーガーメニューの記述
+    const hamburgerButton = document.getElementById("headerButton");
+    const headerLogo = document.querySelector(".header__logo .open")
+
+
+    window.addEventListener('load', function () {
+        // ハンバーガーメニュー
+        hamburgerButton.addEventListener('click', function () {
+            const hamburgerGetClass = hamburgerButton.classList.contains('is-active');
+
+            if (!hamburgerGetClass) {
+                hamburgerButton.classList.add('is-active');
+                headerLogo.classList.add('is-active');
+            } else {
+                hamburgerButton.classList.remove('is-active');
+                headerLogo.classList.remove('is-active');
+            }
+        })
+    })
+}
