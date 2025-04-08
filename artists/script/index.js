0690baebefca5ba0f55fa8c11305f8b349e30998
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-08 19:09:22 +0900

    Add files via upload

---
+++ artists/script/index.js
@@ -0,0 +1,8 @@
+import hamburgerFunc from "./_modules/hamburger.js";
+import accordionFunc from "./_modules/accordion.js";
+import fadeFunc from "./_modules/fade.js";
+//追加で必要な関数
+
+hamburgerFunc();
+accordionFunc();
+fadeFunc();
