5e1f2323173df9329263cc5d9f97e0973e403e7d
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-08 19:07:57 +0900

    Delete artist directory

--- artist/script/index.js
+++
@@ -1,8 +0,0 @@
-import hamburgerFunc from "./_modules/hamburger.js";
-import accordionFunc from "./_modules/accordion.js";
-import fadeFunc from "./_modules/fade.js";
-//追加で必要な関数
-
-hamburgerFunc();
-accordionFunc();
-fadeFunc();
