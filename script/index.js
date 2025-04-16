28d7587594ca43719068e1338e894e7b703b54a0
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-04-16 22:32:33 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -281,23 +281,7 @@ function copyButton() {
 }
 
 function slider() {
-    const businessSwiper = new Swiper(".business__swiper", {
-        loop: true, // スライドをループさせる
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true, // ページネーションをクリック可能に
-        },
-        effect: 'slide', // スライドのエフェクト（デフォルト）
-        speed: 800, // スライドの速度（ミリ秒）
-        centeredSlides: true,//真ん中寄せ
-        slidesPerView: 1.6,//枚数指定
-    });
-
-    const artistSwiper = new Swiper(".artist__swiper", {
+    const newsSwiper = new Swiper(".news__swiper", {
         loop: true, // スライドをループさせる
         // autoplay: {
         //     delay: 3000, // 3秒ごとに自動スライド
