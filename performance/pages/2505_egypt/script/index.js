a405d423dc26352e1e13391b4f6c7e5929eb448e
HatakeyamaKazuma <kazumahatakeyama0128@gmail.com>
2025-05-12 17:29:02 +0900

    Add files via upload

---
+++ performance/pages/2505_egypt/script/index.js
@@ -0,0 +1,97 @@
+import hamburgerFunc from "./_modules/hamburger.js";
+import accordionFunc from "./_modules/accordion.js";
+import fadeFunc from "./_modules/fade.js";
+//追加で必要な関数
+
+hamburgerFunc();
+accordionFunc();
+fadeFunc();
+
+
+
+const loopSwiper = new Swiper(".loopSwiper", {
+    loop: true, // スライドをループさせる
+    autoplay: {
+        delay: 0, // 3秒ごとに自動スライド
+    },
+    effect: 'slide', // スライドのエフェクト（デフォルト）
+    speed: 6000, // スライドの速度（ミリ秒）
+    slidesPerView: 1.5,//枚数指定
+    allowTouchMove: false, // スワイプ無効
+    spaceBetween: '10%',
+
+    breakpoints: {
+        750: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        }
+    },
+});
+
+const egyptSwiper = new Swiper(".egyptSwiper", {
+    pagination: {
+        el: ".swiper-pagination-egypt", // ページネーションのクラス名を指定
+        type: "fraction", // ページネーションのtypeを指定
+    },
+    loop: true, // スライドをループさせる
+    autoplay: {
+        delay: 3000, // 3秒ごとに自動スライド
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'slide', // スライドのエフェクト（デフォルト）
+    speed: 1000, // スライドの速度（ミリ秒）
+    slidesPerView: 1.25,//枚数指定
+    spaceBetween: '20%',
+    centeredSlides: true,
+    breakpoints: {
+        750: {
+            slidesPerView: 1.5,
+            spaceBetween: '8%',
+        }
+    },
+});
+
+
+let articleSwiper = null;
+
+function toggleArticleSwiper() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 750) {
+        // Swiperがまだ初期化されていなければ初期化
+        if (!articleSwiper) {
+            const articleSwiper = new Swiper(".articleSwiper", {
+                loop: true, // スライドをループさせる
+                // autoplay: {
+                //     delay: 3000, // 3秒ごとに自動スライド
+                //     disableOnInteraction: false, // ユーザー操作後も自動再生を継続
+                // },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true, // ページネーションをクリック可能に
+                },
+                effect: 'slide', // スライドのエフェクト（デフォルト）
+                speed: 800, // スライドの速度（ミリ秒）
+                centeredSlides: true,//真ん中寄せ
+                slidesPerView: 1.6,//枚数指定
+            });
+        }
+    } else {
+        // Swiperが有効なら破棄
+        if (articleSwiper) {
+            articleSwiper.destroy(true, true);
+            articleSwiper = null;
+        }
+    }
+}
+
+// ページ読み込み時とリサイズ時に実行
+window.addEventListener('load', toggleArticleSwiper);
+window.addEventListener('resize', toggleArticleSwiper);
